37de8426dcd196a78bf87477f3e54dd521948745
ambernina <rob.ambernina@gmail.com>
2020-03-04 13:13:19 -0700

    added jawsdb stuff to connection and hidden password

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Hakyeon0pp@",
-  database: "burgerDB"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "burgerDB"
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
